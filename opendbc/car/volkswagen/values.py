37eb64cca7df29658a92db10ae416c32862f3cf2
Twenba <twenba@users.noreply.github.com>
2026-09-07 22:43:20 +0800

    tune: raise HCA watchdog threshold for Kodiaq Mk2

--- opendbc/car/volkswagen/values.py
+++ opendbc/car/volkswagen/values.py
@@ -57,7 +57,7 @@ class CarControllerParams:
   AEB_CONTROL_STEP = 2                     # ACC_10 frequency 50Hz
   AEB_HUD_STEP = 20                        # ACC_15 frequency 5Hz
   HCA_STATUS_WATCHDOG_WINDOW_FRAMES = round(5.0 / DT_CTRL)
-  HCA_STATUS_WATCHDOG_ALLOWED_FLUCTUATIONS_PER_SECOND = 2.0
+  HCA_STATUS_WATCHDOG_ALLOWED_FLUCTUATIONS_PER_SECOND = 4.0
 
   # Documented lateral limits: 3.00 Nm max, rate of change 5.00 Nm/sec.
   # MQB vs PQ maximums are shared, but rate-of-change limited differently
